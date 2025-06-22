2e96ca9b55fd9189894e1bafeca7917988266ff4
herr kaste <herr.kaste@gmail.com>
2025-06-22 19:31:57 +0200

    Reorder config.py

--- impl/config.py
+++ impl/config.py
@@ -1,13 +1,13 @@
 import os
 import sublime
 
+BUILD = int(sublime.version())
+PLATFORM = f"{sublime.platform()}-{sublime.arch()}"
 
 INSTALLED_PACKAGES_PATH = sublime.installed_packages_path()
 PACKAGES_PATH = sublime.packages_path()
 CACHE_PATH = sublime.cache_path()
 BACKUP_DIR = os.path.abspath(os.path.join(INSTALLED_PACKAGES_PATH, "..", "Backup"))
-BUILD = int(sublime.version())
-PLATFORM = f"{sublime.platform()}-{sublime.arch()}"
 
 PACKAGE = "Package x Control"
 PACKAGE_SETTINGS = f"{PACKAGE}.sublime-settings"
@@ -17,16 +17,16 @@ PACKAGE_CONTROL_PREFERENCES = "Package Control.sublime-settings"
 PACKAGE_CONTROL_PREFERENCES_LISTENER_KEY = "ad520441-7701-4038-b5a4-c360ac87528d"
 SUBLIME_PREFERENCES = "Preferences.sublime-settings"
 
+PACKAGE_DIR = os.path.join(PACKAGES_PATH, PACKAGE)
+ROOT_DIR = os.path.abspath(os.path.join(CACHE_PATH, "..", "Package Storage", PACKAGE))
+PACKAGES_REPOSITORY = os.path.join(ROOT_DIR, "repository.json")
+CACHE_DIR = os.path.join(CACHE_PATH, PACKAGE)
+PACKAGE_CONTROL_OVERRIDE = os.path.join(CACHE_DIR, "Package Control.sublime-settings")
+
 DEFAULT_CHANNEL = (
     "https://raw.githubusercontent.com/wbond/package_control_channel"
     "/refs/heads/master/channel.json"
 )
-
-CACHE_DIR = os.path.join(CACHE_PATH, PACKAGE)
-ROOT_DIR = os.path.abspath(os.path.join(CACHE_PATH, "..", "Package Storage", PACKAGE))
-PACKAGE_DIR = os.path.join(PACKAGES_PATH, PACKAGE)
-PACKAGES_REPOSITORY = os.path.join(ROOT_DIR, "repository.json")
-PACKAGE_CONTROL_OVERRIDE = os.path.join(CACHE_DIR, "Package Control.sublime-settings")
 REGISTRY_URL = (
     "https://github.com/kaste/Package-x-Control/"
     "releases/download/registry-latest/registry.json"
